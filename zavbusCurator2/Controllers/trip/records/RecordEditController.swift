9c27bde013229018c599d1022e9a23c638ad62fb
wolfhound <v.maslov@ihc.ru>
2018-02-16 13:16:22 +0500

    редактирование оконочательной суммы

--- zavbusCurator2/Controllers/trip/records/RecordEditController.swift
+++ zavbusCurator2/Controllers/trip/records/RecordEditController.swift
@@ -15,6 +15,7 @@ class RecordEditController: UITableViewController {
     @IBOutlet weak var needInsuranceSwitch: UISwitch!
 
     @IBOutlet weak var resultSum: UILabel!
+    @IBOutlet weak var resultSumInput: UITextField!
     @IBOutlet weak var usedBonuses: UILabel!
     @IBOutlet weak var paidBonusesCell: UIView!
 
@@ -23,6 +24,16 @@ class RecordEditController: UITableViewController {
 
         changeProgram(status: statusesIndex[i] as! Int32)
         updateResultSum()
+    }
+
+    @IBAction func resultSumChangeAction(_ sender: UITextField) {
+        var sumForPay: Int32 = 0
+        if (sender.text != "") {
+            sumForPay = Int32.init(sender.text!)!
+        }
+
+        tripRecord?.sumForPay = sumForPay
+        tripRecord?.save()
     }
 
     @IBAction func changeOptionsAction(_ sender: UISwitch) {
@@ -34,15 +45,19 @@ class RecordEditController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        resultSumInput.keyboardType = UIKeyboardType.numberPad
+
         orderedKitNameCell.textLabel?.text = tripRecord?.orderedKit
 
         needStuffSwitch.isOn = (tripRecord?.needStuff)!
         needMealSwitch.isOn = (tripRecord?.needMeal)!
         needInsuranceSwitch.isOn = false
 
-        statusSegment.selectedSegmentIndex = statusesIndex.filter {
-            $0.value == tripRecord!.status
-        }.first?.key as! Int
+        if [4, 5, 6].contains(tripRecord!.status) {
+            statusSegment.selectedSegmentIndex = statusesIndex.filter {
+                $0.value == tripRecord!.status
+            }.first?.key as! Int
+        }
 
         if (tripRecord?.payedBonuses == 0) {
 //            tableView.willRemoveSubview(paidBonusesCell)
@@ -58,6 +73,11 @@ class RecordEditController: UITableViewController {
     }
 
     func changeProgram(status: Int32) {
+        if !([4, 5, 6].contains(tripRecord!.status)) {
+            return
+        }
+
+
         self.tripProgram = tripRecord?.trip?.programs?.filter {
             ($0 as! TripProgram).status == status
         }.first as! TripProgram
@@ -68,23 +88,32 @@ class RecordEditController: UITableViewController {
     }
 
     func updateResultSum() {
+        if !([4, 5, 6].contains(tripRecord!.status)) {
+            return
+        }
         var sumForPay: Int32 = 0
 
-        sumForPay += (tripProgram?.basicPrice)!
+        if (!(tripRecord?.confirmed)!) {
 
-        if (needStuffSwitch.isOn) {
-            sumForPay += (tripProgram?.stuffPrice)!
-        }
-        if (needMealSwitch.isOn) {
-            sumForPay += (tripProgram?.mealPrice)!
-        }
-        sumForPay -= (tripRecord?.payedBonuses)!
-        sumForPay -= (tripRecord?.paidSum)!
+            sumForPay += (tripProgram?.basicPrice)!
 
-        tripRecord?.sumForPay = sumForPay
-        tripRecord?.save()
+            if (needStuffSwitch.isOn) {
+                sumForPay += (tripProgram?.stuffPrice)!
+            }
+            if (needMealSwitch.isOn) {
+                sumForPay += (tripProgram?.mealPrice)!
+            }
+            sumForPay -= (tripRecord?.payedBonuses)!
+            sumForPay -= (tripRecord?.paidSum)!
+
+            tripRecord?.sumForPay = sumForPay
+            tripRecord?.save()
+        } else {
+            sumForPay = (tripRecord?.sumForPay)!
+        }
 
         resultSum.text = "\(sumForPay)"
+        resultSumInput.text = "\(sumForPay)"
     }
 
 
